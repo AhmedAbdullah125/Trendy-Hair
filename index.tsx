bd80f73ebfea7239303771d6908d5a75a4473c71
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2026-06-01 15:12:53 +0300

    chore: remove @tanstack/react-query-devtools from dependencies and index file

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 
 // Create a client
@@ -25,7 +24,6 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
